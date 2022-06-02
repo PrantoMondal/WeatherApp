45e92e2547e86ec703d40b7ee89f5ebede392e58
PrantoMondal <prantomondal517@gmail.com>
2022-06-02 22:32:37 +0600

    Permission and main activity done

--- app/src/main/java/com/pmondal/weather/MainActivity.kt
+++ app/src/main/java/com/pmondal/weather/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.pmondal.weather
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
+
+    val locationPermissionRequest = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        when {
+            permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
+                Toast.makeText(this, "Precise granted", Toast.LENGTH_SHORT).show()
+            }
+            permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
+                Toast.makeText(this, "Approximate granted", Toast.LENGTH_SHORT).show()
+            } else -> {
+            Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        locationPermissionRequest.launch(arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION))
+
     }
 }
