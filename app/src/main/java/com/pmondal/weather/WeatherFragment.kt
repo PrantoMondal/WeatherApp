bfe8190a2788e30d2ec98e1aebf6e48114b8a421
PrantoMondal <prantomondal517@gmail.com>
2022-06-11 22:25:36 +0600

    city search added

--- app/src/main/java/com/pmondal/weather/WeatherFragment.kt
+++ app/src/main/java/com/pmondal/weather/WeatherFragment.kt
@@ -1,12 +1,15 @@
 package com.pmondal.weather
 
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
+import android.view.*
+import android.widget.SearchView
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isNotEmpty
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -18,6 +21,48 @@ import com.pmondal.weather.viewmodels.WeatherViewModel
 class WeatherFragment : Fragment() {
     private lateinit var binding : FragmentWeatherBinding
     private val weatherViewModel:WeatherViewModel by activityViewModels()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.weather_menu,menu)
+        val searchView = menu.findItem(R.id.item_search).actionView as SearchView
+        searchView.queryHint = "Search any city"
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                p0?.let {
+                    convertQueryToLatLong(p0)
+
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                return true
+            }
+
+        })
+    }
+
+    private fun convertQueryToLatLong(p0: String) {
+        val geocoder = Geocoder(requireActivity())
+        val addressList : List<Address> = geocoder.getFromLocationName(p0,1)
+        if (addressList.isNotEmpty()){
+            val lat = addressList[0].latitude
+            val lng = addressList[0].longitude
+            val location = Location("").apply {
+                latitude = lat
+                longitude = lng
+            }
+            weatherViewModel.setNewLocation(location)
+
+        }
+        else{
+            Toast.makeText(requireActivity(), "Invalid city name", Toast.LENGTH_SHORT).show()
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
